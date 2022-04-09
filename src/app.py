521da77af8ec8746cfc3534939faa4a2a4ea6baa
victorlma <victorlima1414x@gmail.com>
2022-04-09 01:06:25 -0300

    Separation between Rendering and Fetching data

--- src/app.py
+++ src/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import randomcamp
 app = Flask(__name__)
 
-@app.route('/', defaults={'path':''})
-@app.route('/<path:path>')
-def main_page(path):
+
+@app.route('/getalbum')
+def get_album():
     random_album_iframe, album_info = randomcamp.get_random_album() 
-    return render_template("base.html", iframe = random_album_iframe, info = album_info)
+    data = { "album_iframe" : random_album_iframe,
+             "album_info" : album_info}
+    return  jsonify(data)
+
+@app.route('/')
+def main_page():
+    return render_template("base.html")
 
 if __name__ == '__main__':
           app.run(host="0.0.0.0",debug=True)
